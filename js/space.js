b1b232beacede43c21fd0e6e5a0cfddf57d80197
Bryon <bryon.pectol@gmail.com>
2019-06-20 17:29:06 -0600

    added space programs

--- js/space.js
+++ js/space.js
@@ -1,7 +1,15 @@
 import { space } from "../js/spacecontent.js";
+import { footer } from "../js/spacecontent.js";
+import { me } from "../js/spacecontent.js";
+
+
+//main
 
 const mainContainer = document.createElement("div");
 mainContainer.className = "testp";
+
+
+document.body.appendChild(mainContainer);
 
 const allCars = space.map(myCar => {
     let carsYeah = space.find(CAR => {
@@ -17,7 +25,7 @@ const allCars = space.map(myCar => {
   allCars.forEach(myCar => {
    
   
-    let makeElement = document.createElement("a");
+    let makeElement = document.createElement("button");
   
   
     makeElement.className = "testp1";
@@ -27,8 +35,83 @@ const allCars = space.map(myCar => {
     makeElement.textContent = myCar.mAKE;
   
     mainContainer.appendChild(makeElement);
+    
+  });
+
+
+//footer
+
+
+  const footerContainer = document.createElement("div");
+  footerContainer.className = "testp2";
+
+  document.body.appendChild(footerContainer);
+
+
+  const allfooters = footer.map(myFooter => {
+    let carsFooter = footer.find(Footer => {
+      return Footer.name === myFooter.name;
+    });
+  
+    return {
+      FoOtEr: carsFooter.footer,
+  
+    };
+  });
+
+  allfooters.forEach(myFooter => {
+   
+  
+    let footerElement = document.createElement("p");
+  
+  
+    footerElement.className = "testfooter";
+
+  
+  
+    footerElement.textContent = myFooter.FoOtEr;
+  
+    footerContainer.appendChild(footerElement);
+    
+  });
+
+
+//me
+
+
+  const meContainer = document.createElement("div");
+  meContainer.className = "testp2";
+
+  document.body.appendChild(meContainer);
+
+
+  const allMe = me.map(myMe => {
+    let carsMe = me.find(Me => {
+      return Me.name === myMe.name;
+    });
+  
+    return {
+      mE: carsMe.me
+  
+    };
+  });
+
+  allMe.forEach(myMe => {
+   
+  
+    let meElement = document.createElement("p");
+  
+    meElement.className = "testfooter";
+  
+    meElement.textContent = myMe.mE;
+  
+    footerContainer.appendChild(meElement);
+    
   });
   
-  document.body.appendChild(mainContainer);
+  
+  
   
   console.log(space);
+  console.log(footer);
+  console.log(me);
--- js/spacecontent.js
+++ js/spacecontent.js
@@ -1,8 +1,47 @@
 export const space = [
     {
-    "name": "NASA",
+    "name": "SpaceX"
     },
     {
-    "name": "SpaceX"
+    "name": "Chinese National Space Agency"
+    },
+    {
+    "name": "European Space Agency"
+    },
+    {
+    "name": "Indian Space Research Organization"
+    },
+    {
+    "name": "National Aeronautics and Space Administration"
+    },
+    {
+    "name": "Soviet Space Program/Roscomos"
+    },
+    {
+    "name": "Orbital Sciences"
+    },
+    {
+    "name": "Blue Origin"
+    },
+    {
+    "name": "Bigelow Aerospace"
+    },
+    {
+    "name": "SpaceDev/Sierra Nevada Corp."
+    },
+    {
+    "name": "Virgin Galactic"
+    }
+]
+
+export const footer = [
+    {
+        "footer": "Powered by VS Code and Netlify.com, made by:"
+    }
+]
+
+export const me = [
+    {
+        "me": "Bryon Pectol"
     }
 ]
